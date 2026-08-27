39950e489db67c91325aead150ccb9a2714356bd
Animesh Kumar <animesh.kumar9651@gmail.com>
2026-08-27 18:37:36 +0530

    api issue fixed

--- core/settings.py
+++ core/settings.py
@@ -132,23 +132,3 @@ CORS_ALLOWED_ORIGINS = [
     "https://xlr8-frontend.vercel.app", 
 ]
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'WARNING',
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-    },
-}
